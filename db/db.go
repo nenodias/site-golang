1ebb0f439d036acdb1b49ad2957d867a09f9744e
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-05-30 20:40:14 -0300

    Updating project by using sql connection

--- db/db.go
+++ db/db.go
@@ -2,13 +2,15 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"regexp"
 
 	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/nenodias/site-golang/model"
 )
 
-func GetConnection() (*sql.DB, error) {
+func Init() {
 	regex := func(re, s string) (bool, error) {
 		return regexp.MatchString(re, s)
 	}
@@ -18,16 +20,81 @@ func GetConnection() (*sql.DB, error) {
 				return conn.RegisterFunc("regexp", regex, true)
 			},
 		})
+}
+
+func GetConnection() (*sql.DB, error) {
 	return sql.Open("sqlite3_extended", "./banco.db")
 }
 
 func Create(conn *sql.DB) error {
 	_, err := conn.Exec(`CREATE TABLE produto(
-			id int64 NOT NULL,
+			id int64 NOT NULL PRIMARY KEY,
 			nome varchar(100) NOT NULL,
 			descricao varchar(255) NOT NULL,
-			valor float NOT NULL,
+			preco float NOT NULL,
 			quantidade NOT NULL
 		)`, nil)
 	return err
+}
+
+func Insert(conn *sql.DB, produto *model.Produto) error {
+	tx, err := conn.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(`INSERT INTO produto(
+			id,
+			nome,
+			descricao,
+			preco,
+			quantidade
+		) VALUES(? , ?, ?, ?, ?) `, produto.Id, produto.Nome, produto.Descricao, produto.Preco, produto.Quantidade)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+func Update(conn *sql.DB, produto *model.Produto) error {
+	tx, err := conn.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(`UPDATE produto SET
+			nome = ?,
+			descricao = ?,
+			preco = ?,
+			quantidade = ?
+		WHERE id = ?`, produto.Nome, produto.Descricao, produto.Preco, produto.Quantidade, produto.Id)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+func SelectById(conn *sql.DB, id int) (*model.Produto, error) {
+	produto := model.Produto{}
+	res := conn.QueryRow("SELECT * FROM produto WHERE id = ? ", id)
+	err := res.Scan(&produto.Id, &produto.Nome, &produto.Descricao, &produto.Preco, &produto.Quantidade)
+	if err != nil {
+		return nil, err
+	}
+	return &produto, nil
+}
+
+func SelectAll(conn *sql.DB) []model.Produto {
+	retorno := []model.Produto{}
+	res, err := conn.Query("SELECT * FROM produto", nil)
+	if err == nil {
+		for res.Next() {
+			produto := model.Produto{}
+			err := res.Scan(&produto.Id, &produto.Nome, &produto.Descricao, &produto.Preco, &produto.Quantidade)
+			if err != nil {
+				fmt.Print(err)
+				break
+			}
+			retorno = append(retorno, produto)
+		}
+	}
+	return retorno
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -9,43 +10,24 @@ import (
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
-
-type Produto struct {
-	Id         int64
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
+var conn *sql.DB
 
 func main() {
-	conn, err := db.GetConnection()
+	db.Init()
+	var err error
+	conn, err = db.GetConnection()
 	if err != nil {
 		fmt.Println("Erro na conexao")
 		panic(err)
 	}
-	err = db.Create(conn)
-	if err != nil {
-		fmt.Println("Erro no create")
-	}
-	res, err := conn.Query("SELECT * FROM produto")
-	if err != nil {
-		fmt.Println("Erro no select")
-		panic(err)
-	}
-	fmt.Println(res.Columns())
-	fmt.Println(res.Next())
+
 	http.HandleFunc("/", index)
 	fmt.Println(temp)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
-	}
+	produtos := db.SelectAll(conn)
+	fmt.Println(produtos)
 	temp.ExecuteTemplate(w, "Index", produtos)
 }
