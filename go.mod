45f714f4aecf26772075048da403502b7721f919
nenodias <horacio.dias92@gmail.com>
2022-05-30 13:48:38 -0300

    Adding products by struct into web

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nenodias/site-golang
+
+go 1.17
--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	fmt.Println(temp)
@@ -15,5 +22,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
